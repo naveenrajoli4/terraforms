0be34e94930a55cbd156d9caa3875b250af5b671
naveenrajoli04 <naveenrajoli04@gmail.com>
2025-02-18 15:55:47 +0530

    yes

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -4,24 +4,3 @@ resource "aws_instance" "my_instance" {
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   tags = var.ec2_tags
 }
-
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic and outbound traffic"
-
-  ingress {
-    from_port   = var.from_to_port
-    to_port     = var.from_to_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = "0"
-    to_port     = "0"
-    protocol    = "-1"
-    cidr_blocks = var.cidr_blocks
-  }
-
-  tags = var.sg_name
-}
